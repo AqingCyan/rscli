538ff6b0e628e5d937983f5c6eee8cf5bb73f340
AqingCyan <aqingcyan@gmail.com>
2024-04-24 20:32:42 +0800

    feat: 基础的 cli 结构

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+// rscli csv -i input.csv -o output.json --header -d ','
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name = "rscli", version, author, about, long_about)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: Subcommand,
+}
+
+#[derive(Debug, Parser)]
+enum Subcommand {
+    #[command(name = "csv", about = "Show CSV, or Convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long)]
+    input: String,
+
+    #[arg(short, long, default_value = "output.json")]
+    output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+
+    #[arg(long, default_value_t = true)]
+    header: bool,
+}
+
 fn main() {
-    println!("Hello, rust!");
+    let opts = Opts::parse();
+    println!("{:?}", opts);
 }
