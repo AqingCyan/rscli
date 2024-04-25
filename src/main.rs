30e172c09ca1dbe222d2dbec68f69ddc39e49a25
AqingCyan <aqingcyan@gmail.com>
2024-04-25 15:32:21 +0800

    refactor: 重构代码

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,11 @@
-use std::fs;
-use std::path::Path;
-
 use clap::Parser;
-use csv::Reader;
-use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-struct Player {
-    name: String,
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
-
-#[derive(Debug, Parser)]
-#[command(name = "rscli", version, author, about, long_about)]
-struct Opts {
-    #[command(subcommand)]
-    cmd: Subcommand,
-}
-
-#[derive(Debug, Parser)]
-enum Subcommand {
-    #[command(name = "csv", about = "Show CSV, or Convert CSV to other formats")]
-    Csv(CsvOpts),
-}
-
-#[derive(Debug, Parser)]
-struct CsvOpts {
-    #[arg(short, long, value_parser = verify_input_file)]
-    input: String,
-
-    #[arg(short, long, default_value = "output.json")]
-    output: String,
-
-    #[arg(short, long, default_value_t = ',')]
-    delimiter: char,
-
-    #[arg(long, default_value_t = true)]
-    header: bool,
-}
+use rscli::{process_csv, Opts, Subcommand};
 
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     match opts.cmd {
-        Subcommand::Csv(opts) => {
-            let mut reader = Reader::from_path(opts.input)?;
-            let mut ret = Vec::with_capacity(128);
-            for result in reader.deserialize() {
-                let player: Player = result?;
-                ret.push(player);
-            }
-
-            let json = serde_json::to_string_pretty(&ret)?;
-            fs::write(opts.output, json)?;
-        }
+        Subcommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
     }
-
     Ok(())
-}
-
-fn verify_input_file(filename: &str) -> Result<String, String> {
-    if Path::new(filename).exists() {
-        Ok(filename.into())
-    } else {
-        Err("File does not exist".into())
-    }
 }
