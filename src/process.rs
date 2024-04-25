fb1f7e6eb55331caa859977f8c21cf6772962899
AqingCyan <aqingcyan@gmail.com>
2024-04-25 18:22:08 +0800

    refactor: 不依赖具体的数据结构来解析 CSV

--- src/process.rs
+++ src/process.rs
@@ -2,30 +2,17 @@ use std::fs;
 
 use anyhow::Result;
 use csv::Reader;
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-struct Player {
-    name: String,
-
-    position: String,
-
-    #[serde(rename = "DOB")]
-    dob: String,
-
-    nationality: String,
-
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
+use serde_json::Value;
 
 pub fn process_csv(input: &str, output: &str) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(128);
-    for result in reader.deserialize() {
-        let player: Player = result?;
-        ret.push(player);
+    let headers = reader.headers()?.clone();
+
+    for result in reader.records() {
+        let record = result?;
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        ret.push(json_value);
     }
 
     let json = serde_json::to_string_pretty(&ret)?;
